88f2dc00c1ee0f369a8c73093d79c552aaf25375
sbangapadang22 <sbangapadang@mines.edu>
2024-02-16 15:01:29 -0700

    changed mouth height to 11

--- C11C-DrawPlay/src/main/Cat.java
+++ C11C-DrawPlay/src/main/Cat.java
@@ -1,3 +1,5 @@
+package main;
+
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.geom.*;
@@ -14,7 +16,7 @@ public class Cat {
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
+	private static final int MOUTH_HEIGHT = 11;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
